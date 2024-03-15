fa8c99df972b4819014f0a4a740034d36a26124f
udo.zuhause.laptop <udo.zuhause.laptop>
2024-03-15 20:37:50 +0100

    adapt path to repository name

--- create_user_cfg_sym_links.sh
+++ create_user_cfg_sym_links.sh
@@ -10,10 +10,10 @@
 
 # bash, zsh, vim, mutt
 # pacman -S  zsh, vim, mutt
-ln -sv ~/Config/home/.bashrc ~/.bashrc
-ln -sv ~/Config/home/.git-prompt.sh ~/.git-prompt.sh
-ln -sv ~/Config/home/.vimrc ~/.bashrc
-ln -sv ~/Config/home/.muttrc ~/.muttrc
+ln -sv ~/linux_config/home/.bashrc ~/.bashrc
+ln -sv ~/linux_config/home/.git-prompt.sh ~/.git-prompt.sh
+ln -sv ~/linux_config/home/.vimrc ~/.bashrc
+ln -sv ~/linux_config/home/.muttrc ~/.muttrc
 
 # Hyprland
 # pacman -S hyprland, hyprpaper, waybar, kitty, ttf-0xproto-nerd
@@ -21,11 +21,11 @@ mkdir -v ~/.config/hypr
 mkdir -v ~/.config/waybar
 mkdir -v ~/.config/kitty
 
-ln -sv ~/Config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sv ~/Config/hypr/hyprpaper.conf ~/.config/kitty/kitty.conf
-ln -sv ~/Config/waybar/config ~/.config/waybar/config
-ln -sv ~/Config/waybar/style.css  ~/.config/waybar/style.css
-ln -sv ~/Config/.conf ~/.config/kitty/kitty.conf
+ln -sv ~/linux_config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sv ~/linux_config/hypr/hyprpaper.conf ~/.config/kitty/kitty.conf
+ln -sv ~/linux_config/waybar/config ~/.config/waybar/config
+ln -sv ~/linux_config/waybar/style.css  ~/.config/waybar/style.css
+ln -sv ~/linux_config/.conf ~/.config/kitty/kitty.conf
 
 # "install" vim pluginmanager
 #mkdir -p ~/.vim/autoload
